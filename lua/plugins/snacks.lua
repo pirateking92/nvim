890cc4c87954ea0d00c346d2d899614629a656cf
Matt Doyle <doyle9214@gmail.com>
2025-04-09 14:19:39 +0100

    having an issue with gg and G. don't seem to be working properly. think its an issue with which-key

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,10 +1,20 @@
 return {
-  "snacks.nvim",
-  opts = function(_, opts)
-    table.insert(
-      opts.dashboard.preset.keys,
-      7,
-      { icon = "S", key = "S", desc = "Select Session", action = require("persistence").select }
-    )
-  end,
+  "folke/snacks.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = false },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
 }
